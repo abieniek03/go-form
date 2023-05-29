4ae05a8d0d3447d10a393d87b447c088d24c19a5
Adrian Bieniek <a.bieniek.kontakt@gmail.com>
2023-05-29 21:30:54 +0200

    change background url

--- src/components/elements/Header.vue
+++ src/components/elements/Header.vue
@@ -5,7 +5,7 @@ import Button from './Button.vue';
 <template>
 	<header class="relative h-screen w-full flex justify-center items-center z-0">
 		<div
-			class="h-full w-full bg-center bg-cover bg-no-repeat bg-[url('http://localhost:5173/src/assets/header-mobile.jpg')] bg-gray-700 bg-blend-multiply before:absolute before:h-screen before:w-full before:bg-gradient-to-t before:from-background before:to-transparent lg:bg-[url('http://localhost:5173/src/assets/header-desktop.jpg')]"
+			class="h-full w-full bg-center bg-cover bg-no-repeat bg-[url('https://go-form.vercel.app/src/assets/header-mobile.jpg')] bg-gray-700 bg-blend-multiply before:absolute before:h-screen before:w-full before:bg-gradient-to-t before:from-background before:to-transparent lg:bg-[url('https://go-form.vercel.app/src/assets/header-desktop.jpg')]"
 		></div>
 		<div class="absolute z-10 py-8 px-6 mx-auto max-w-screen-xl text-center lg:py-16">
 			<h1 class="mb-2 text-white text-4xl font-bold tracking-tight leading-none md:text-5xl lg:mb-4 lg:text-6xl">
